8577c2e1e2738dbf1bee468ec09c4c93aa92daba
suttonkc35 <122035352+suttonkc35@users.noreply.github.com>
2023-01-13 16:38:08 -0500

    Add files via upload

---
+++ alert1.js
@@ -0,0 +1,14 @@
+var Fname = window.prompt("Enter your name: ");
+alert(`Welcome ${Fname}!`);
+
+
+var myFavNum = window.prompt("What is your favorite number?")
+
+
+const myPi = 3.1415926
+
+var area = parseInt(myFavNum) * parseInt(myFavNum) * parseInt(myPi);
+
+alert(`Hello ${Fname} you have entered ${myFavNum} as your favorite number. If that was the radius of a circle, the circle's area would be ${area}!`);
+
+
